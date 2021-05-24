9b6c47154ecef00a69a1313755f1897915be4513
geborenik <geborenik@geborenikui-MacBookAir.local>
2021-05-24 21:33:36 +0900

    Add : 폴더 구조 추가

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Main from './Main';
+import Main from './pages/Main';
 
 class Routes extends Component {
   render() {
