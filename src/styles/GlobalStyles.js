75fb84a4fbd854696fe32add32565d77423046f3
geborenik <geborenik@geborenikui-MacBookAir.local>
2021-05-24 20:45:50 +0900

    WIP

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,8 +1,17 @@
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset"; // style-reset 패키지
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset'; // style-reset 패키지
 
 const GlobalStyles = createGlobalStyle` 
 ${reset}
+
+
+
+
+
+
+
+
+
 * {
   box-sizing: border-box;
   margin: 0;
